dad53837678fe30794ca59fb21d508ee8f448e73
JuanPabloDaza <97971860+JuanPabloDaza@users.noreply.github.com>
2024-03-08 14:45:29 +0000

    Implementacion Tests

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -1,10 +1,34 @@
 from yatzy import Yatzy
 
 # These unit tests can be run using the py.test framework
-# available from http://pytest.org/
+# available from http://pytest.org/ 
 
-
-def test_demo():
+def test_chance():
+        actual = Yatzy.chance(1, 2, 3, 4, 5)
         expected = 15
-        actual = 15
-        assert expected == actual        
+        assert actual == expected
+
+def test_yatzy_sameNumber():
+        assert 50 == Yatzy.yatzy([4,4,4,4,4])
+        assert 50 == Yatzy.yatzy([6,6,6,6,6])
+
+def test_yatzy_oneDiferentNumber():
+        assert 0 == Yatzy.yatzy([4,4,4,4,5])
+        assert 0 == Yatzy.yatzy([6,6,6,6,3])
+
+def test_ones_atLeastOne():
+        numbers = [1,2,1,3,4]
+        expected = 2
+        actual = Yatzy.ones(*numbers)
+        assert actual == expected
+
+def test_ones_noOne():
+        numbers = [2,2,4,3,4]
+        expected = 0
+        actual = Yatzy.ones(*numbers)
+        assert actual == expected
+
+def test_twoPair():
+        
+
+
